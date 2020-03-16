397a08c2f84fbb52222817b236a8069f28dc5df5
Pedro del Rio <pedelriomarron@gmail.com>
2020-03-16 23:37:38 +0100

    Updates

---
+++ precache-manifest.5a0a078d5a678624ac23296d2ae748d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75adaaee44bdc82e0183b2ece1284bdc",
+    "url": "/meme-app_react/index.html"
+  },
+  {
+    "revision": "d702818bd7f3455d92d4",
+    "url": "/meme-app_react/static/css/main.691b68b2.chunk.css"
+  },
+  {
+    "revision": "0f894e812b38f9575be8",
+    "url": "/meme-app_react/static/js/2.936e0df7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/meme-app_react/static/js/2.936e0df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d702818bd7f3455d92d4",
+    "url": "/meme-app_react/static/js/main.a19892cf.chunk.js"
+  },
+  {
+    "revision": "7c900520b160e7fd6e1e",
+    "url": "/meme-app_react/static/js/runtime-main.18d74bc1.js"
+  }
+]);
